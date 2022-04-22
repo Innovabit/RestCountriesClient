ad2d10ea4e3b63cadac9616e940d5202a25783f7
Juan Vega <jcvegan@live.com>
2022-04-22 07:38:16 -0500

    Adding run tests when push to feature, bugfix or hotfix branches

--- src/Innovabit.DotNet.Api.RestCountries/Models/CountryName.cs
+++ src/Innovabit.DotNet.Api.RestCountries/Models/CountryName.cs
@@ -4,6 +4,6 @@ namespace Innovabit.DotNet.Api.RestCountries.Models
 {
     public class CountryName : Name
     {
-        public IReadOnlyDictionary<string, Name> NativeName { get; set; }
+        public IReadOnlyDictionary<string, Name> NativeName { get; set; } = new Dictionary<string, Name>();
     }
 }
