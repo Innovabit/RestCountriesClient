786e919fd28a8ac5fd7046029bfe625436c37dbf
Juan Vega <jcvegan@live.com>
2022-04-22 18:13:32 -0500

    Removing AssemblyInfo.cs and adding AssemblyAttribute in the csproj file.

--- src/Innovabit.DotNet.Api.RestCountries/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-[assembly: System.Reflection.AssemblyCompanyAttribute("Innovabit.DotNet.Api.RestCountries")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Innovabit.DotNet.Api.RestCountries")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Innovabit.DotNet.Api.RestCountries")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Innovabit.DotNet.Api.RestCountries.Tests")]
